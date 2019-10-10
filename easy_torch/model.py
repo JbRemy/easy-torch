394b063990e41823bbeda2068908e3563d40c122
JbRemy <remyjeanb@gmail.com>
2019-10-10 18:09:21 +0200

    added correct model

--- easy_torch/model.py
+++ easy_torch/model.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Sequence
 
 import os
 from copy import copy
@@ -12,9 +12,35 @@ import numpy as np
 
 from .network import Network
 from .callbacks import schedulers, _callback
-from .helpers.torch import get_device, set_seed
+from .helpers.torch import get_device, set_seed, send_to
+
+# TODO: We need to define the syntax for network definition somewhere.
+# TODO: Probably define a more proper way to store the metrics
 
 class Model(object):
+    """The overlord class to manage and train neural networks.
+
+    The public attributes are the ones that need to be saved when saving the
+    model.
+    
+    Attributes:
+        layers (list): A list of layers following the correct syntax.
+        device (torch.device)
+        criteron (str)
+        optimizer_name (str)
+        optimizer_kwargs (dict)
+        training_seed (int)
+        initialization_seed (int)
+    Methods:
+        __init__(self, layers: List[str], device: str="auto", 
+                 seed: Optional[int]=None) -> None:
+           Defines architectural parameters of the network.
+        compile(self, optimizer: str, criteron: str, 
+                optimizer_kwargs: Optional[dict]=None, 
+                criteron_kwargs: Optional[dict]=None) -> None:
+            Defines the training aspects of the model.
+
+    """
     def __init__(self, layers: List[str], device: str="auto", 
                  seed: Optional[int]=None) -> None:
         """Defines architectural parameters of the network.
@@ -26,12 +52,18 @@ class Model(object):
             layers (list): A list of layers following the correct syntax.
             device (str): On which device sould the model be send. "cpu",
                 "gpu", "auto".
+                (default: "auto")
             seed (int): The seed for initialisation of the parameters.
         """
-        self._layers = layers
-        self._device = get_device(device)
-        self._initialisation_seed = seed if seed else np.random.randint()
-        if seed and self._device == torch.device("cuda"):
+        self.layers = layers
+        self.device = get_device(device)
+        self.initialization_seed = seed if seed else np.random.randint()
+        self.criteron = None
+        self.optimizer_name = None
+        self.optimizer_kwargs = None
+        self.training_seed = None
+        # TODO @Simon: Properly manage warnings ? 
+        if seed and self.device == torch.device("cuda"):
             if not torch.backends.cudnn.deterministic or torch.backends.cudnn.benchmark:
                 warning.warn(
                     """
@@ -43,6 +75,7 @@ class Model(object):
                     """,
                     Warning
                 )
+
 
     def compile(self, optimizer: str, criteron: str, 
                 optimizer_kwargs: Optional[dict]=None, 
@@ -62,24 +95,141 @@ class Model(object):
             optimizer_kwargs (dict): arguments for said optimizer.
             criteron_kwargs (dict): arguments for said criteron.
         """
-        self._criteron = getattr(nn, criteron)(**criteron_kwargs)
-        self._optimizer_name = optimizer
-        self._optimizer_kwargs = optimizer_kwargs
+        self.criteron = getattr(nn, criteron)(**criteron_kwargs)
+        self.optimizer_name = optimizer
+        self.optimizer_kwargs = optimizer_kwargs
 
     def train(self, train_loader: torch.utils.data.DataLoader, 
               epochs: int,
               log_folder: Optional[str]=None, 
-              log_freq: Optional[int]=100, 
+              log_freq: Optional[int]=None, 
               test_loader: Optional[torch.utils.data.DataLoader]=None, 
-              test_freq: Optional[int]=1000,
+              test_freq: Optional[int]=None,
               callbacks: Optional[list[Union[str, _callback._CallBack]]]=None) -> None:
+        # TODO: Find a way to pass strings callbacks and initializes them
+        # automatically, in a User friendly way. For now you need to init them
+        # before calling train.
         """Trains the network
 
+        Args:
+            train_loader (torch.utils.data.DataLoader): A data loader for
+                training data.
+            epochs (int): The total number of epochs.
+            log_folder (str): Where to log training data, and save the model if
+                needed.
+            log_freq (int): Frequency of logging results (in iterations).
+            test_loader (torch.utils.data.DataLoader): A data loader for
+                testing data.
+            test_freq (int): Test frequency.
+            callbacks (list): A list of callbacks to apply, already
+                instanciated.
         """
-        set_seed(self._seed)
-        self._network = Network(self._layers, self._device, train_loader_SHAPE)
-        self._optimizer = getattr(torch.optim, self._optimizer_name)(
+        self._initialize_weights_and_optimizer(train_loader_SHAPE) # TODO train_loader_SHAPE
+        self._initialize_callbacks(callbacks)
+        self.record = {}
+
+        for epoch in epochs:
+            for i, (data, target) in enumerate(train_loader):
+                if i & test_freq == 0:
+                    self._test(test_loader):
+
+                jump = data.size(0)
+                self._data, self._target = send_to([data, target], self.device)
+                self._train_data, self._train_target = send_to([test_data, test_target], self.device)
+                self._train_output = self.network(self._train_data)
+                self._train_loss = criteron(self._train_output, self._train_target)
+                self._call_callbacks("train", jump)
+
+    def _test(self, loader) -> None:
+        """One pass on the test dataset
+
+        Args:
+            loader (torch.utils.data.DataLoader): A data loader for testing data.
+        """
+        for i, (test_data, test_target) in enumerate(loader):
+            test_jump = data.size(0)
+            self._test_data, self._test_target = send_to([test_data, test_target], self.device)
+            self._test_output = self.network(self._test_data)
+            self._test_loss = criteron(self._test_output, self._test_target)
+            self._call_callbacks("test", test_jump)
+
+    def _call_callbacks(self, time: "str"):
+        """Calls all reqiured callbacks
+
+        Args:
+            time (str): "test" or "train"
+        """
+        for callback in getattr(self, "_%s_callbacks" % time):
+            args = [getattr(self, _) for _ in callbacks.REQUIRED_ARGS]
+            self._store_res(callback.__name__, callbacks.iteration_end(jump, args))
+
+    def _store_res(self, name: str, res: Sequence):
+        """Stores results in self.record if needed, at the iteration address
+
+        Args:
+            name (str): the name of the result
+            res (iterable): the metrics to store
+        """
+        # TODO @Simon: What do you think about this ? The point is that you dont
+        # check if name exists.
+        for res_ in res:
+            if res_:
+                try:
+                    self.record[name][iterations] = res_
+
+                except KeyError:
+                    self.record[name] = {}
+                    self.record[name][iterations] = res_
+
+    def _initialize_weights_and_optimizer(self, input_shape: Sequence[int]) -> None:
+        """Builds the Network, initializes it's weights, and creates the
+        optimizer.
+
+        Args:
+            input_shape (list, tuple): The input shape of the data.
+        """
+        set_seed(self.initialisation_seed)
+        self._network = Network(self.layers, self.device, input_shape) 
+        self._optimizer = getattr(torch.optim, self.optimizer_name)(
             self.network.parameters(),
-            **self._optimizer_kwargs
+            **self.optimizer_kwargs
         )
+
+    def _initialize_callbacks(self, callbacks) -> None:
+        """Build all the required callbacks
+
+        Args:
+            callbacks: Optional[list[Union[str, _callback._CallBack]]]
+        """
+        self._test_callbacks = [callback in callbacks if callback.CALL_AT == "test"] 
+        self._train_callbacks = [callback in callbacks if callback.CALL_AT == "train"] 
+
+    with tqdm(total=len(train_loader)*train_loader.batch_size, unit_scale=True,
+              desc="Epoch %i/%i" % (epoch+1, epochs),
+              postfix={"train_acc": 0.0, "test_acc": 0.0}) as pbar:
+
+    pbar.update(data.size(0))
+    pbar.set_postfix({"train_acc": self._logger.train_acc,
+                      "test_acc": self._logger.test_acc})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
