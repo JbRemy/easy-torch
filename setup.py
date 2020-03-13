7702fb7f98e586737284258be5a0e0205c40cc27
JbRemy <remyjeanb@gmail.com>
2020-03-13 15:30:43 +0100

    new reqs

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='easy-torch',  
-     version='0.1',
-     packages=['easy-torch'] ,
-     author="Jean-Baptiste Remy",
-     author_email="remyjeanb@gmail.com",
-     description="A package for easy prototyping and management of Pytorch models",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/JbRemy/easy-torch",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
+    name='easy-torch',  
+    version='0.1',
+    author="Jean-Baptiste Remy",
+    author_email="remyjeanb@gmail.com",
+    description="A package for easy prototyping and management of Pytorch models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JbRemy/easy-torch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
  )
